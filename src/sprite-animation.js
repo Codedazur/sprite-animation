6f3344ee2608faceb9907e0f8509a395175db49b
rick.ekelschot <rick.ekelschot@codedazur.nl>
2014-05-05 11:33:33 +0200

    New build 05-05-2014

--- src/sprite-animation.js
+++ src/sprite-animation.js
@@ -363,11 +363,7 @@
         canvasSupported = function () {
             if (document.createElement('canvas').getContext === undefined) {
                 return false;
-            } else {
-                return true;
-            }
-
-            if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(GT-P5110)|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
+            } else if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(GT-P5110)|(Windows Phone (OS 7))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
                 return false;
             }
 
